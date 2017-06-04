82cf116d355c121fc1de7aac15ccf8f3f0392228
JensPauwels <jens.pauwels@hotmail.com>
2017-06-04 17:48:54 +0200

    added

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
-console.log('working');
+const express = require('express');
+const bodyParser = require('body-parser');
+const app = express();
+app.listen(8080,() => {
+  console.log('listening on port 8080');
+});
+app.use(express.static('public'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+
+
+
+app.get('/',(req, res) => {
+  res.redirect('/index.html');
+});
