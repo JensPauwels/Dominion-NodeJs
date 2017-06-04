67c8363a53fd66bf13b1c382a0a6ecea996f29f7
JensPauwels <jens.pauwels@hotmail.com>
2017-06-04 16:14:20 +0000

    lol

--- script.js
+++ script.js
@@ -1,9 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
-app.listen(5000,() => {
-  console.log('listening on port 8080');
-});
+app.set('port', (process.env.PORT  || 80);
 app.use(express.static('public'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
