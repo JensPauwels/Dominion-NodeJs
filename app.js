08bff1542ee0c7b858c618a67d8f20005d0ad4ed
Jens Pauwels <jenspauwels@Jenss-MBP.home>
2017-07-14 23:00:57 +0200

    please

--- app.js
+++ app.js
@@ -2,17 +2,56 @@ const express = require('express');
 const app = express();
 const server = require('http').createServer(app);
 const io = require('socket.io').listen(server);
+const connection = require('./mysql');
+const users = [];
 
 
-server.listen(process.env.PORT || 80);
+server.listen(process.env.PORT || 9999);
 console.log('Server running...');
-app.get('/',(req, res) => {
-  res.sendFile(__dirname + '/index.html');
-});
+
+const getUserNames = function () {
+  let userNames = [];
+  users.forEach(user => {
+    userNames.push(user.username);
+  });
+  return userNames;
+};
+
+const logout = function (socket) {
+  let indexToRemove = '';
+  users.forEach((user, index) => {
+    if (user.socket === socket) indexToRemove = index;
+  });
+  if (indexToRemove !== '') {
+    users.splice(indexToRemove,1);
+    socket.broadcast.emit('updateUserList',getUserNames());
+  }
+};
 
 io.sockets.on('connection', (socket) => {
-  socket.on('sendMessage', (obj) => {
-    console.log(obj);
-    socket.broadcast.emit('newMessage',obj);
+  socket.on('login', (obj) => {
+    connection.getUser(obj.username,obj.password,(loggedIn) => {
+      let objToReturn = {};
+      if (!loggedIn) objToReturn = {status: loggedIn};
+      else {
+        users.push({username: obj.username,socket});
+        socket.broadcast.emit('updateUserList', getUserNames());
+        objToReturn = {
+          username: obj.username,
+          status: 'online',
+          userList: getUserNames()
+        };
+      }
+      socket.emit('loginStatus', objToReturn);
+    });
+  });
+
+  socket.on('logout',() => {
+    logout(socket);
+  });
+
+
+  socket.on('disconnect', () => {
+    logout(socket);
   });
 });
