80e7f5b394604c67ed0ff63e24573d7bff554af0
Jens Pauwels <jenspauwels@Jenss-MacBook-Pro.local>
2017-09-22 15:24:37 +0200

    test

--- tmp/init/server.js
+++ tmp/init/server.js
@@ -43,6 +43,7 @@ io.sockets.on('connection', (socket) => {
 
     if (user.time < 5000 + new Date().getTime()) {
       socket.broadcast.emit('updateClients', {msg: xss(msg), user});
+      user.time = new Date().getTime();
     }
 
   });
